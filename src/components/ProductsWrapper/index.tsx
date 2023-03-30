cddbaea99f7c0ecd5f0424bb22af58050eb1a0b8
GLGIO7 <giovanelucas00@outlook.com>
2023-03-30 17:53:16 -0300

    update: code organizing

--- src/components/ProductsWrapper/index.tsx
+++ src/components/ProductsWrapper/index.tsx
@@ -1,10 +1,6 @@
 import ProductCard from "../ProductCard";
 import * as S from "./styles";
 
-type ProductsWrapperProps = {
-	title: string;
-};
-
 interface IProduct {
 	name: string;
 	price: string;
@@ -12,108 +8,30 @@ interface IProduct {
 	image_path: string;
 }
 
-interface IProductsList {
-	[key: string]: IProduct[];
-}
-
-const productsList: IProductsList = {
-	star_wars: [
-		{
-			name: `Produto ${Math.round(Math.random() * 100)}`,
-			price: `R$ ${Math.round(Math.random() * 100)}`,
-			href: "/",
-			image_path: "/img/sw1.jpg",
-		},
-		{
-			name: `Produto ${Math.round(Math.random() * 100)}`,
-			price: `R$ ${Math.round(Math.random() * 100)}`,
-			href: "/",
-			image_path: "/img/sw2.jpg",
-		},
-		{
-			name: `Produto ${Math.round(Math.random() * 100)}`,
-			price: `R$ ${Math.round(Math.random() * 100)}`,
-			href: "/",
-			image_path: "/img/sw3.jpg",
-		},
-		{
-			name: `Produto ${Math.round(Math.random() * 100)}`,
-			price: `R$ ${Math.round(Math.random() * 100)}`,
-			href: "/",
-			image_path: "/img/sw4.jpg",
-		},
-		{
-			name: `Produto ${Math.round(Math.random() * 100)}`,
-			price: `R$ ${Math.round(Math.random() * 100)}`,
-			href: "/",
-			image_path: "/img/sw5.jpg",
-		},
-		{
-			name: `Produto ${Math.round(Math.random() * 100)}`,
-			price: `R$ ${Math.round(Math.random() * 100)}`,
-			href: "/",
-			image_path: "/img/sw6.jpg",
-		},
-	],
-	consoles: [
-		{
-			name: `Produto ${Math.round(Math.random() * 100)}`,
-			price: `R$ ${Math.round(Math.random() * 100)}`,
-			href: "/",
-			image_path: "/img/cons1.jpg",
-		},
-		{
-			name: `Produto ${Math.round(Math.random() * 100)}`,
-			price: `R$ ${Math.round(Math.random() * 100)}`,
-			href: "/",
-			image_path: "/img/cons2.jpg",
-		},
-		{
-			name: `Produto ${Math.round(Math.random() * 100)}`,
-			price: `R$ ${Math.round(Math.random() * 100)}`,
-			href: "/",
-			image_path: "/img/cons3.jpg",
-		},
-		{
-			name: `Produto ${Math.round(Math.random() * 100)}`,
-			price: `R$ ${Math.round(Math.random() * 100)}`,
-			href: "/",
-			image_path: "/img/cons4.jpg",
-		},
-		{
-			name: `Produto ${Math.round(Math.random() * 100)}`,
-			price: `R$ ${Math.round(Math.random() * 100)}`,
-			href: "/",
-			image_path: "/img/cons5.jpg",
-		},
-		{
-			name: `Produto ${Math.round(Math.random() * 100)}`,
-			price: `R$ ${Math.round(Math.random() * 100)}`,
-			href: "/",
-			image_path: "/img/cons6.jpg",
-		},
-	],
+type ProductsWrapperProps = {
+	title: string;
+	productList: IProduct[];
 };
 
-const ProductsWrapper = ({ title }: ProductsWrapperProps) => {
-	const listTitle: string = title === "star_wars" ? "Star Wars" : title;
-
+const ProductsWrapper = ({ title, productList }: ProductsWrapperProps) => {
 	return (
 		<S.Wrapper>
 			<h2 className="wrapper__header">
-				<span>{listTitle}</span>
+				<span>{title}</span>
 				<a href="/">Ver mais →</a>
 			</h2>
-			{productsList[title].map((item, index) => {
-				return (
-					<ProductCard
-						name={item.name}
-						price={item.price}
-						imagePath={item.image_path}
-						key={index}
-					/>
-				);
-			})}
+			{productList &&
+				productList.map((item, index) => {
+					if (item.price)
+						return (
+							<ProductCard
+								name={item.name}
+								price={item.price}
+								imagePath={item.image_path}
+								key={index}
+							/>
+						);
+				})}
 		</S.Wrapper>
 	);
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,112 @@
 import Hero from "../components/Hero";
 import ProductsWrapper from "../components/ProductsWrapper";
 
+interface IProduct {
+	title?: string;
+	name: string;
+	price: string;
+	href: string;
+	image_path: string;
+}
+
+interface IProductsLists {
+	[key: string]: IProduct[];
+}
+
+const productsLists: IProductsLists = {
+	star_wars: [
+		{ title: "Star Wars", name: "", price: "", href: "", image_path: "" },
+		{
+			name: `Produto ${Math.round(Math.random() * 100)}`,
+			price: `R$ ${Math.round(Math.random() * 100)}`,
+			href: "/",
+			image_path: "/img/sw1.jpg",
+		},
+		{
+			name: `Produto ${Math.round(Math.random() * 100)}`,
+			price: `R$ ${Math.round(Math.random() * 100)}`,
+			href: "/",
+			image_path: "/img/sw2.jpg",
+		},
+		{
+			name: `Produto ${Math.round(Math.random() * 100)}`,
+			price: `R$ ${Math.round(Math.random() * 100)}`,
+			href: "/",
+			image_path: "/img/sw3.jpg",
+		},
+		{
+			name: `Produto ${Math.round(Math.random() * 100)}`,
+			price: `R$ ${Math.round(Math.random() * 100)}`,
+			href: "/",
+			image_path: "/img/sw4.jpg",
+		},
+		{
+			name: `Produto ${Math.round(Math.random() * 100)}`,
+			price: `R$ ${Math.round(Math.random() * 100)}`,
+			href: "/",
+			image_path: "/img/sw5.jpg",
+		},
+		{
+			name: `Produto ${Math.round(Math.random() * 100)}`,
+			price: `R$ ${Math.round(Math.random() * 100)}`,
+			href: "/",
+			image_path: "/img/sw6.jpg",
+		},
+	],
+	consoles: [
+		{ title: "Consoles", name: "", price: "", href: "", image_path: "" },
+		{
+			name: `Produto ${Math.round(Math.random() * 100)}`,
+			price: `R$ ${Math.round(Math.random() * 100)}`,
+			href: "/",
+			image_path: "/img/cons1.jpg",
+		},
+		{
+			name: `Produto ${Math.round(Math.random() * 100)}`,
+			price: `R$ ${Math.round(Math.random() * 100)}`,
+			href: "/",
+			image_path: "/img/cons2.jpg",
+		},
+		{
+			name: `Produto ${Math.round(Math.random() * 100)}`,
+			price: `R$ ${Math.round(Math.random() * 100)}`,
+			href: "/",
+			image_path: "/img/cons3.jpg",
+		},
+		{
+			name: `Produto ${Math.round(Math.random() * 100)}`,
+			price: `R$ ${Math.round(Math.random() * 100)}`,
+			href: "/",
+			image_path: "/img/cons4.jpg",
+		},
+		{
+			name: `Produto ${Math.round(Math.random() * 100)}`,
+			price: `R$ ${Math.round(Math.random() * 100)}`,
+			href: "/",
+			image_path: "/img/cons5.jpg",
+		},
+		{
+			name: `Produto ${Math.round(Math.random() * 100)}`,
+			price: `R$ ${Math.round(Math.random() * 100)}`,
+			href: "/",
+			image_path: "/img/cons6.jpg",
+		},
+	],
+};
+
 const Home = () => {
 	return (
 		<>
 			<Hero />
-			<ProductsWrapper title="star_wars" />
-			<ProductsWrapper title="consoles" />
-			<ProductsWrapper title="star_wars" />
-			<ProductsWrapper title="consoles" />
+			{Object.values(productsLists).map((item, index) => {
+				return (
+					<ProductsWrapper
+						key={index}
+						title={item[0].title || ""}
+						productList={item}
+					/>
+				);
+			})}
 		</>
 	);
 };
