b86e7b312efc5c72c50d111c05e9f40a6b656e3f
GLGIO7 <giovanelucas00@outlook.com>
2023-03-31 23:18:09 -0300

    testing: vercel...

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const routes = Router();
 
-routes.get("/list/products", (req, res) => {
+routes.get("/products", (req, res) => {
 	return res.status(200).json({ message: "Produtos aparecerão aqui" });
 });
 
