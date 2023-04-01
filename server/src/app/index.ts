ead86e96e53276c3bfac17fecaf69d5257f584ed
GLGIO7 <giovanelucas00@outlook.com>
2023-03-31 22:47:54 -0300

    testing: backend vercel deploy

--- server/src/app/index.ts
+++ server/src/app/index.ts
@@ -1,11 +1,9 @@
 import express from "express";
 import "dotenv/config";
 import routes from "../routes";
-import path from "path";
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
 app.use(routes);
 
