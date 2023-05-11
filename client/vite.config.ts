33564241e1ee084e906f60b46d9cf40d78d3985a
GLGIO7 <giovanelucas00@outlook.com>
2023-05-11 05:31:00 -0300

    fix: some configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,12 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	build: {
-		outDir: "../dist/client",
-		assetsDir: ".",
-		rollupOptions: {
-			input: {
-				main: "../dist/client/index.html",
-			},
-		},
+		outDir: "../dist",
 	},
 });
