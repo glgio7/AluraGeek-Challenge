6ffa9f70d72aeae0d03071f5fa862a254ddd2d96
GLGIO7 <giovanelucas00@outlook.com>
2023-05-11 04:59:59 -0300

    fix: vercel & vite configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,11 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	build: {
-		outDir: "../dist/client",
-		assetsDir: ".",
+		outDir: "dist",
 		rollupOptions: {
 			input: {
-				main: "../dist/client/index.html",
+				main: "./src/index.html",
 			},
 		},
 	},
